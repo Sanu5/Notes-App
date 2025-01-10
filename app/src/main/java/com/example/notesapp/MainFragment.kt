2336785329a634a005eba33b61407374d462cfb8
Anish Biswal <anish@aimor.xyz>
2025-01-10 14:40:48 +0530

    worked

--- app/src/main/java/com/example/notesapp/MainFragment.kt
+++ app/src/main/java/com/example/notesapp/MainFragment.kt
@@ -31,7 +31,7 @@ class MainFragment : Fragment() {
         }
 
 
-        return inflater.inflate(R.layout.fragment_main, container, true)
+        return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
 
