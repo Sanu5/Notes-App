f15af827767cc89847b813a62522c116b37e661f
Anish Biswal <anish@aimor.xyz>
2025-01-10 14:40:19 +0530

    checking github desktop

--- app/src/main/java/com/example/notesapp/MainFragment.kt
+++ app/src/main/java/com/example/notesapp/MainFragment.kt
@@ -31,7 +31,7 @@ class MainFragment : Fragment() {
         }
 
 
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        return inflater.inflate(R.layout.fragment_main, container, true)
     }
 
 
